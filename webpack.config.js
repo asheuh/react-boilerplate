6bb311725daa2d7263311fd0d3f5ee1ce56ffdf6
asheuh <asheuh49@gmail.com>
2018-12-19 21:00:40 +0300

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
