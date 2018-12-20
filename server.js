a1349a7d980818cd6243b9e9d7560255d88ffbbf
asheuh <asheuh49@gmail.com>
2018-12-20 06:46:11 +0300

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+
+
 app.use(express.static(__dirname + '/'));
 app.get('*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './src/index.html'));
